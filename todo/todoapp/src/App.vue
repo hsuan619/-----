6998c4c256929ef2dc76d835661943ae01f721b6
hsuan619 <b11023055@gemail.yuntech.edu.tw>
2022-06-27 13:59:10 +0800

    gg

---
+++ todo/todoapp/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <todo-app></todo-app>
+  </div>
+</template>
+
+<script>
+
+//import todoapp from './components/todoapp'
+
+export default {
+  name: 'App',
+  components: {
+    //todoapp
+  }
+}
+</script>
+
+
---
+++ todo/todoapp/src/components/todoapp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h2 class="text-center mt-5">My Vue Todo App</h2>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
